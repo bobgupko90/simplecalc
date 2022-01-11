d091d37ec17e134016aead451c9a5a0c12f34691
Yulia <yulia.kramar@hotmail.com>
2022-01-11 11:35:04 +0000

    Update Tests.cs

--- SimpleCalculator.Tests/Tests.cs
+++ SimpleCalculator.Tests/Tests.cs
@@ -1,4 +1,4 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 
 
 namespace SimpleCalculator.Tests
@@ -7,9 +7,29 @@ namespace SimpleCalculator.Tests
     public class Tests
     {
         [Test]
-        public void Pass()
+         public void Add_WithOneAndOne_ReturnsTwo()
         {
-            Assert.Pass();            
+            var result = SimpleCalculator.Add(1, 1);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(1, 0, ExpectedResult = 1)]
+        [TestCase(0, 1, ExpectedResult = 1)]
+        [TestCase(1, 1, ExpectedResult = 2)]
+        public int Add(int x, int y)
+        {
+            return SimpleCalculator.Add(x, y);
+        }
+
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(1, 0, ExpectedResult = 1)]
+        [TestCase(0, 1, ExpectedResult = -1)]
+        [TestCase(1, 1, ExpectedResult = 0)]
+        public int Sub(int x, int y)
+        {
+            return SimpleCalculator.Sub(x, y);
         }
     }
 }
